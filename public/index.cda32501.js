69ef9de0a8933257bd1da669a3cbb6c8ed055b04
Jonathan Lucke <luckejonathan2@gmail.com>
2023-02-10 15:32:28 -0500

    deleted all files from public directory while fiddling with build

--- public/index.cda32501.js
+++
@@ -1,2 +0,0 @@
-const e=document.createElement("button");e.innerHTML="PEACE be with you",document.getElementById("main").appendChild(e),e.addEventListener("click",(()=>{const n=new XMLHttpRequest;n.onload=function(){e.innerHTML="check your console log.",console.log(JSON.parse(n.response))},n.open("GET","test",!0),n.send()}));
-//# sourceMappingURL=index.cda32501.js.map
